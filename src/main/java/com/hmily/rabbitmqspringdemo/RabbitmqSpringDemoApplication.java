533cc608d7f0954f68f672f2f6179970d2e64e6b
oushiming <oushiming@sxt.com>
2018-08-12 23:18:34 +0800

    init

---
+++ src/main/java/com/hmily/rabbitmqspringdemo/RabbitmqSpringDemoApplication.java
@@ -0,0 +1,12 @@
+package com.hmily.rabbitmqspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqSpringDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hmily/rabbitmqspringdemo/RabbitmqSpringDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hmily.rabbitmqspringdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RabbitmqSpringDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
