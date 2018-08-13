116a9607080a4871d4a37f22f2327590fd5e7dc3
oushiming <oushiming@sxt.com>
2018-08-13 23:46:28 +0800

    init

---
+++ src/main/java/com/hmily/rabbitmqspringdemo/common/RabbitMQConfig.java
@@ -0,0 +1,100 @@
+package com.hmily.rabbitmqspringdemo.common;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"com.hmily.rabbitmqspringdemo.common.*"})
+public class RabbitMQConfig {
+
+    public final static String RABBITMQ_HOST = "192.168.0.7";
+    public final static int RABBITMQ_PORT = 5672;
+    public final static String RABBITMQ_DEFAULT_VIRTUAL_HOST = "/";
+    public final static String RABBITMQ_USERNAME = "guest";
+    public final static String RABBITMQ_PASSWORD = "guest";
+
+    @Bean
+    public ConnectionFactory connectionFactory(){
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setAddresses(RABBITMQ_HOST);
+        connectionFactory.setUsername(RABBITMQ_USERNAME);
+        connectionFactory.setPassword(RABBITMQ_PASSWORD);
+        connectionFactory.setVirtualHost(RABBITMQ_DEFAULT_VIRTUAL_HOST);
+        return connectionFactory;
+    }
+
+    @Bean //这里的connectionFactory要和上面的方法名一致！！！！
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
+        rabbitAdmin.setAutoStartup(true);
+        return rabbitAdmin;
+    }
+
+    /**
+     * 针对消费者配置
+     * 1. 设置交换机类型
+     * 2. 将队列绑定到交换机
+     FanoutExchange: 将消息分发到所有的绑定队列，无routingkey的概念
+     HeadersExchange ：通过添加属性key-value匹配
+     DirectExchange:按照routingkey分发到指定队列
+     TopicExchange:多关键字匹配
+     */
+    @Bean
+    public TopicExchange exchange001() {
+        return new TopicExchange("topic001", true, false);
+    }
+
+    @Bean
+    public Queue queue001() {
+        return new Queue("queue001", true); //队列持久
+    }
+
+    @Bean
+    public Binding binding001() {
+        return BindingBuilder.bind(queue001()).to(exchange001()).with("spring.*");
+    }
+
+    @Bean
+    public TopicExchange exchange002() {
+        return new TopicExchange("topic002", true, false);
+    }
+
+    @Bean
+    public Queue queue002() {
+        return new Queue("queue002", true); //队列持久
+    }
+
+    @Bean
+    public Binding binding002() {
+        return BindingBuilder.bind(queue002()).to(exchange002()).with("rabbit.*");
+    }
+
+    @Bean
+    public Queue queue003() {
+        return new Queue("queue003", true); //队列持久
+    }
+
+    @Bean
+    public Binding binding003() {
+        return BindingBuilder.bind(queue003()).to(exchange001()).with("mq.*");
+    }
+
+    @Bean
+    public Queue queue_image() {
+        return new Queue("image_queue", true); //队列持久
+    }
+
+    @Bean
+    public Queue queue_pdf() {
+        return new Queue("pdf_queue", true); //队列持久
+    }
+
+}
